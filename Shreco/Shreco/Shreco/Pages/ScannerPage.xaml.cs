ac7203f5314ad3cb0647bd3376852624f5d48fe0
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-03-27 14:28:43 +0300

    Change Scanner

---
+++ Shreco/Shreco/Shreco/Pages/ScannerPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Shreco.Pages
+{	
+	public partial class ScannerPage : ContentPage
+	{	
+		public ScannerPage ()
+		{
+			InitializeComponent ();
+
+		}
+
+		public delegate void ScanMethods(string result);
+		public event ScanMethods OnScanResult;
+
+		async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+			await Navigation.PopModalAsync();
+        }
+
+        void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+			OnScanResult?.Invoke(result.ToString());
+        }
+    }
+}
+
--- Shreco/Shreco/Shreco/Views/QrScanButtonView.xaml.cs
+++ Shreco/Shreco/Shreco/Views/QrScanButtonView.xaml.cs
@@ -7,11 +7,11 @@ public partial class QrScanButtonView : ContentView {
     public event ScanMethods OnScanResult;
     private async void OpenScanner_Clicked(object sender, EventArgs e)
     {
-        var scanPage = new ZXingScannerPage();
-        await Application.Current.MainPage.Navigation.PushModalAsync(scanPage, true);
-        scanPage.OnScanResult += async (result) => {
-            scanPage.IsScanning = false;
-            Device.BeginInvokeOnMainThread(async () => {
+        ScannerPage scannerPage = new();
+        await Navigation.PushModalAsync(scannerPage, true);
+        scannerPage.OnScanResult += (result) => {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
                 OnScanResult?.Invoke(result.ToString());
                 await Application.Current.MainPage.Navigation.PopModalAsync();
             });
